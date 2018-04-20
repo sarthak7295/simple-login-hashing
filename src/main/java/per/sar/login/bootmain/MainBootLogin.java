2be6d5bff24f8b6409995b32c0cebc28d332b43d
sarthak7295 <sarthak.rawat@yahoo.com>
2018-04-20 22:04:09 +0530

    fin test

--- src/main/java/per/sar/login/bootmain/MainBootLogin.java
+++ src/main/java/per/sar/login/bootmain/MainBootLogin.java
@@ -4,16 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.SpringServletContainerInitializer;
+
 
 
 @SpringBootApplication
 @EnableJpaRepositories("per.sar.login.*")
 @ComponentScan(basePackages = { "per.sar.login.*" })
 @EntityScan("per.sar.login.*")   
-public class MainBootLogin extends SpringServletContainerInitializer{
+public class MainBootLogin extends SpringBootServletInitializer{
 
 	@Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
